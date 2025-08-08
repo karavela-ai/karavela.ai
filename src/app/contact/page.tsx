96e2d4131954f695e5aa3757b1e78a5e4c33d5ca
Alexis Thual <13835654+alexisthual@users.noreply.github.com>
2025-08-08 17:30:20 +0200

    Enable actually sending emails

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,186 @@
+'use client'
+
+import { Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/20/solid'
+import { CheckCircleIcon, ExclamationCircleIcon } from '@heroicons/react/24/outline'
+import { useState } from 'react'
+
 import { Container } from '@/componentsNew/container'
 import { Footer } from '@/componentsNew/footer'
 import { Navbar } from '@/componentsNew/navbar'
+import { set } from 'sanity'
 
-function Contact() {
+function NotificationSuccess({
+  show,
+  setShow,
+}: {
+  show: boolean
+  setShow: (show: boolean) => void
+}) {
+  return (
+    <>
+      {/* Global notification live region, render this permanently at the end of the document */}
+      <div
+        aria-live="assertive"
+        className="pointer-events-none fixed inset-0 z-100 flex items-end px-4 py-6 sm:items-start sm:p-6"
+      >
+        <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
+          {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
+          <Transition show={show}>
+            <div className="pointer-events-auto w-full max-w-sm rounded-lg bg-white shadow-lg outline-1 outline-black/5 transition data-closed:opacity-0 data-enter:transform data-enter:duration-300 data-enter:ease-out data-closed:data-enter:translate-y-2 data-leave:duration-100 data-leave:ease-in data-closed:data-enter:sm:translate-x-2 data-closed:data-enter:sm:translate-y-0">
+              <div className="p-4">
+                <div className="flex items-start">
+                  <div className="shrink-0">
+                    <CheckCircleIcon
+                      aria-hidden="true"
+                      className="size-6 text-green-400"
+                    />
+                  </div>
+                  <div className="ml-3 w-0 flex-1 pt-0.5">
+                    <p className="text-sm font-medium text-gray-900">
+                      Email sent!
+                    </p>
+                    <p className="mt-1 text-sm text-gray-500">
+                      Thank you for reaching out, we will get back to you
+                      shortly.
+                    </p>
+                  </div>
+                  <div className="ml-4 flex shrink-0">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        setShow(false)
+                      }}
+                      className="inline-flex rounded-md text-gray-400 hover:text-gray-500 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600"
+                    >
+                      <span className="sr-only">Close</span>
+                      <XMarkIcon aria-hidden="true" className="size-5" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+    </>
+  )
+}
+
+function NotificationError({
+  show,
+  setShow,
+}: {
+  show: boolean
+  setShow: (show: boolean) => void
+}) {
+  return (
+    <>
+      {/* Global notification live region, render this permanently at the end of the document */}
+      <div
+        aria-live="assertive"
+        className="pointer-events-none fixed inset-0 z-100 flex items-end px-4 py-6 sm:items-start sm:p-6"
+      >
+        <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
+          {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
+          <Transition show={show}>
+            <div className="pointer-events-auto w-full max-w-sm rounded-lg bg-white shadow-lg outline-1 outline-black/5 transition data-closed:opacity-0 data-enter:transform data-enter:duration-300 data-enter:ease-out data-closed:data-enter:translate-y-2 data-leave:duration-100 data-leave:ease-in data-closed:data-enter:sm:translate-x-2 data-closed:data-enter:sm:translate-y-0">
+              <div className="p-4">
+                <div className="flex items-start">
+                  <div className="shrink-0">
+                    <ExclamationCircleIcon
+                      aria-hidden="true"
+                      className="size-6 text-red-400"
+                    />
+                  </div>
+                  <div className="ml-3 w-0 flex-1 pt-0.5">
+                    <p className="text-sm font-medium text-gray-900">
+                      Something went wrong
+                    </p>
+                    <p className="mt-1 text-sm text-gray-500">
+                      We encountered an error while sending your message. Please
+                      try again later.
+                    </p>
+                  </div>
+                  <div className="ml-4 flex shrink-0">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        setShow(false)
+                      }}
+                      className="inline-flex rounded-md text-gray-400 hover:text-gray-500 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600"
+                    >
+                      <span className="sr-only">Close</span>
+                      <XMarkIcon aria-hidden="true" className="size-5" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+function Contact({
+  setShowSuccess, setShowError
+}: {
+  setShowSuccess: (show: boolean) => void,
+  setShowError: (show: boolean) => void
+}) {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [subject, setSubject] = useState('')
+  const [message, setMessage] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    setLoading(true)
+    const formData = new FormData(e.currentTarget)
+    const data = {
+      firstName: formData.get('first-name'),
+      lastName: formData.get('last-name'),
+      email: formData.get('email'),
+      subject: formData.get('subject'),
+      message: formData.get('message'),
+    }
+
+    try {
+      const response = await fetch('/api/send', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+
+      const result = await response.json()
+      console.log('Email sent successfully:', result)
+      // Optionally reset form fields
+      setFirstName('')
+      setLastName('')
+      setEmail('')
+      setSubject('')
+      setMessage('')
+      setShowSuccess(true)
+    } catch (error) {
+      console.error('Error sending email:', error)
+      setShowError(true)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div
@@ -22,14 +200,16 @@ function Contact() {
           Get in touch
         </h2>
         <p className="mt-2 text-lg/8 text-gray-600">
-          Whether you are an individual interested in participating in forefront brain research, a research center open to data acquisition
+          Whether you are an individual interested in participating in forefront
+          brain research, a research center open to data acquisition
           partnerships, a pharmaceutical actor seeking for clinical insights, an
           investor looking for your next venture, feel free to reach out to us!
         </p>
       </div>
       <form
-        action="#"
+        action="/api/send"
         method="POST"
+        onSubmit={handleSubmit}
         className="mx-auto mt-16 max-w-xl sm:mt-20"
       >
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
@@ -47,6 +227,8 @@ function Contact() {
                 type="text"
                 autoComplete="given-name"
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
               />
             </div>
           </div>
@@ -65,6 +247,8 @@ function Contact() {
                 type="text"
                 autoComplete="family-name"
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
               />
             </div>
           </div>
@@ -83,24 +267,28 @@ function Contact() {
                 type="email"
                 autoComplete="email"
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
           </div>
 
           <div className="sm:col-span-2">
             <label
-              htmlFor="object"
+              htmlFor="subject"
               className="block text-sm/6 font-semibold text-gray-900"
             >
-              Object
+              Subject
             </label>
             <div className="mt-2.5">
               <input
-                id="object"
-                name="object"
+                id="subject"
+                name="subject"
                 type="text"
                 autoComplete="off"
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
               />
             </div>
           </div>
@@ -118,7 +306,8 @@ function Contact() {
                 name="message"
                 rows={4}
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-                defaultValue={''}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
               />
             </div>
           </div>
@@ -127,9 +316,39 @@ function Contact() {
         <div className="mt-10">
           <button
             type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="flex w-full items-center justify-center gap-3 rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-indigo-200"
+            disabled={
+              !firstName ||
+              !lastName ||
+              !email ||
+              !subject ||
+              !message ||
+              loading
+            }
           >
-            Let’s talk
+            {loading ? (
+              <svg
+                className="mr-3 -ml-1 size-5 animate-spin text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            ) : null}
+            <span>Let’s talk</span>
           </button>
         </div>
       </form>
@@ -138,13 +357,24 @@ function Contact() {
 }
 
 export default function ContactPage() {
+  const [showSuccess, setShowSuccess] = useState(false)
+  const [showError, setShowError] = useState(false)
+
   return (
     <main className="overflow-hidden">
       <Container>
         <Navbar />
       </Container>
-      <Contact />
+      <Contact setShowSuccess={setShowSuccess} setShowError={setShowError} />
       <Footer />
+      <NotificationSuccess
+        show={showSuccess}
+        setShow={setShowSuccess}
+      />
+      <NotificationError
+        show={showError}
+        setShow={setShowError}
+      />
     </main>
   )
 }
