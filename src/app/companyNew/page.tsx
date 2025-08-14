49b5b796e07447aa4b28ea68a028e7aec69c561f
Alexis Thual <13835654+alexisthual@users.noreply.github.com>
2025-08-14 10:41:37 +0200

    Update company page

--- src/app/companyNew/page.tsx
+++ src/app/companyNew/page.tsx
@@ -182,7 +182,7 @@ const values = [
   {
     name: 'Be transparent.',
     description:
-      'We believe that scientific knowledge, just like a good dessert, is better when shared.',
+      'We believe that scientific and data, just like chocolate, are better when shared.',
     icon: ChatBubbleLeftRightIcon,
   },
   {
@@ -242,12 +242,12 @@ const team = [
     imageUrl:
       '/team/antoine-collas.jpeg',
   },
-  // {
-  //   name: 'Louis Jalouzot',
-  //   role: 'Models team scientist',
-  //   imageUrl:
-  //     '/team/louis-jalouzot.jpeg',
-  // },
+  {
+    name: 'Louis Jalouzot',
+    role: 'Models team scientist',
+    imageUrl:
+      '/team/louis-jalouzot.jpeg',
+  },
   {
     name: 'Charles Maussion',
     role: 'Co-founder / CPO',
